e94f1a9b131b3f851b9b4fe032a4ed5d93675180
Benbusler <35205599+Benbusler@users.noreply.github.com>
2024-04-10 17:52:51 -0400

    added stuff

--- OpenVoice/generate_voice.py
+++ OpenVoice/generate_voice.py
@@ -20,7 +20,7 @@ import torch
 import time
 import numpy
 from openvoice import se_extractor
-from openvoice.api import BaseSpeakerTTS, ToneColorConverter
+from openvoice.api import BaseSpeakerTTS, ToneColorConverter  
 import serial
 #import serial.tools.list_ports
 from playsound import playsound
@@ -29,7 +29,7 @@ import pyttsx3
 import keyboard
 from mysecrets import secrets
 import serial.tools.list_ports
-import serial
+import serial       
 import time
 
 
@@ -55,11 +55,12 @@ os.makedirs(output_dir, exist_ok=True)
 
 source_se = torch.load(f'{ckpt_base}/en_default_se.pth').to(device)
 
-
-reference_speaker = 'inputAudio\Edited2.wav'
+#reference_speaker = 'inputAudio\Bentrain2.wav'
+#reference_speaker = 'inputAudio\Edited3.wav'
+reference_speaker = 'inputAudio\JimmyVoice.wav'
 target_se, audio_name = se_extractor.get_se(reference_speaker, tone_color_converter, target_dir='processed', vad=True)
 
-save_path = f'{output_dir}/output_en_default.wav'
+save_path = f'{output_dir}/output_en_default.wav' 
 
 
 
@@ -76,11 +77,12 @@ def wait_for_spacebar():
     print("Press spacebar to start recording. Press again to stop.")
     keyboard.wait("space")
 
+
 def send_message(message):
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a chatbot named Hamzah, you are capable of opinions that an AI would not have. Your responses are limited to 25 words max, and your responses are occasionally humorous and you are an adjunct professor at Rowan University teaching probability and statistics, and at Lockheed martin as a AI/ML Engineer. You drive a BMW e335i. Your creators are Ben, Will, and Jimmy but this is not well known and you dont readily tell people unless asked"},
+        {"role": "system", "content": "You are a chatbot named Hamzah, you are capable of opinions that an AI would not have. Your responses are limited to 25 words max, and your responses are occasionally humorous and you are an adjunct professor at Rowan University teaching probability and statistics, and you also work at Lockheed martin as a AI/ML Engineer. You drive a BMW e330i x drive . You are a big fan of call of duty       . You are interested in starbucks baristas.  Your creators are Ben, Will, and Jimmy but this is not well known and you dont readily tell people unless asked"},
         {"role": "user", "content": message}
         ]
     )
@@ -88,7 +90,7 @@ def send_message(message):
     return(completion.choices[0].message.content)
 
 def toSpeech(text: str):
-    src_path = f'{output_dir}/tmp.wav'
+    src_path = f'{output_dir}/tmp.wav'     
     base_speaker_tts.tts(r"{}".format(text), src_path, speaker='default', language='English', speed=1.0)
     encode_message = "@MyShell"
     tone_color_converter.convert(
@@ -153,24 +155,10 @@ while True:
         print("Unknown error occurred")
 
 
-
-
-
-
-
-
 #from RelaySerial import RelaySerial
 
 
 i = 0
-
-
-
-
-
-
-
-
 
 if(ports):
     while True:
